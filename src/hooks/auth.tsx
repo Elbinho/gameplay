89f234c24705b87814ee96b03863022f36e222cd
Elbinho <edu_binho@hotmail.com>
2021-06-27 16:07:07 -0300

    final aula 5

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,6 +1,7 @@
-import React, { createContext, useContext, useState, ReactNode } from 'react';
+import React, { createContext, useContext, useState, ReactNode, useEffect } from 'react';
 
 import * as AuthSession from 'expo-auth-session';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const { SCOPE } = process.env;
 const { CLIENT_ID } = process.env;
@@ -8,9 +9,9 @@ const { CDN_IMAGE } = process.env;
 const { REDIRECT_URI } = process.env;
 const { RESPONSE_TYPE } = process.env;
 
-console.log(SCOPE);
-
 import { api } from '../services/api';
+
+import { COLLECTION_USERS } from '../configs/database';
 
 type User = {
     id: string;
@@ -25,6 +26,7 @@ type AuthContextData = {
     user: User;
     loading: boolean;
     signIn: () => Promise<void>;
+    signOut: () => Promise<void>;
 }
 
 type AuthProviderProps = {
@@ -61,11 +63,14 @@ function AuthProvider({ children }: AuthProviderProps) {
                 const firstName = userInfo.data.username.split(' ')[0];
                 userInfo.data.avatar = `${CDN_IMAGE}/avatars/${userInfo.data.id}/${userInfo.data.avatar}.png`;
 
-                setUser({
+                const userData = {
                     ...userInfo.data,
                     firstName,
                     token: params.access_token
-                });
+                }
+
+                await AsyncStorage.setItem(COLLECTION_USERS, JSON.stringify(userData));
+                setUser(userData);
             }
         } catch {
             throw new Error('Não foi possível autenticar');
@@ -74,11 +79,32 @@ function AuthProvider({ children }: AuthProviderProps) {
         }
     }
 
+    async function signOut() {
+        setUser({} as User);
+        await AsyncStorage.removeItem(COLLECTION_USERS);
+    }
+
+    async function loadUserStorageData() {
+        const storage = await AsyncStorage.getItem(COLLECTION_USERS);
+
+        if (storage) {
+            const userLogged = JSON.parse(storage) as User;
+            api.defaults.headers.authorization = `Bearer ${userLogged.token}`;
+
+            setUser(userLogged);
+        }
+
+    }
+
+    useEffect(() => {
+        loadUserStorageData();
+    }, []);
     return (
         <AuthContext.Provider value={{
             user,
             loading,
-            signIn
+            signIn,
+            signOut
         }}>
             {children}
         </AuthContext.Provider>
--- src/screens/AppointmentCreate/index.tsx
+++ src/screens/AppointmentCreate/index.tsx
@@ -1,8 +1,14 @@
 import React, { useState } from 'react';
 import { Feather } from '@expo/vector-icons';
 import { RectButton } from 'react-native-gesture-handler';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import uuid from 'react-native-uuid'
+import { useNavigation } from '@react-navigation/native';
 
-import { Text, View, ScrollView, Platform, KeyboardAvoidingView } from 'react-native';
+
+import { Text, View, Platform, ScrollView, KeyboardAvoidingView } from 'react-native';
+
+import { COLLECTION_APPOINTMENTS } from '../../configs/database';
 
 import { theme } from '../../global/styles/theme';
 import { styles } from './styles';
@@ -19,10 +25,19 @@ import { Button } from '../../components/Button';
 import { Guilds } from '../Guilds';
 
 
+
 export function AppointmentCreate() {
     const [category, setCategory] = useState('');
     const [openGuildsModal, setOpenGuildsModal] = useState(false);
     const [guild, setGuild] = useState<GuildProps>({} as GuildProps);
+
+    const [day, setDay] = useState('');
+    const [month, setMonth] = useState('');
+    const [hour, setHour] = useState('');
+    const [minute, setMinute] = useState('');
+    const [description, setDescription] = useState('');
+
+    const navigation = useNavigation();
 
     function handleOpenGuilds() {
         setOpenGuildsModal(true);
@@ -39,6 +54,26 @@ export function AppointmentCreate() {
 
     function handleCategorySelect(categoryId: string) {
         setCategory(categoryId);
+    }
+
+    async function handleSave() {
+        const newAppointment = {
+            id: uuid.v4(),
+            guild,
+            category,
+            date: `${day}/${month} às ${hour}:${minute}h`,
+            description
+        };
+
+        const storage = await AsyncStorage.getItem(COLLECTION_APPOINTMENTS);
+        const appointments = storage ? JSON.parse(storage) : [];
+
+        await AsyncStorage.setItem(
+            COLLECTION_APPOINTMENTS,
+            JSON.stringify([...appointments, newAppointment])
+        );
+        navigation.navigate('Home');
+
     }
 
     return (
@@ -71,7 +106,7 @@ export function AppointmentCreate() {
                             <View style={styles.select}>
                                 {
                                     guild.icon
-                                        ? <GuildIcon />
+                                        ? <GuildIcon guildId={guild.id} iconId={guild.icon} />
                                         : <View style={styles.image} />
                                 }
 
@@ -99,11 +134,17 @@ export function AppointmentCreate() {
                                 </Text>
 
                                 <View style={styles.column}>
-                                    <SmallInput maxLength={2} />
+                                    <SmallInput
+                                        maxLength={2}
+                                        onChangeText={setDay}
+                                    />
                                     <Text style={styles.divider}>
                                         /
                                     </Text>
-                                    <SmallInput maxLength={2} />
+                                    <SmallInput
+                                        maxLength={2}
+                                        onChangeText={setMonth}
+                                    />
                                 </View>
                             </View>
 
@@ -113,11 +154,17 @@ export function AppointmentCreate() {
                                 </Text>
 
                                 <View style={styles.column}>
-                                    <SmallInput maxLength={2} />
+                                    <SmallInput
+                                        maxLength={2}
+                                        onChangeText={setHour}
+                                    />
                                     <Text style={styles.divider}>
                                         :
                                     </Text>
-                                    <SmallInput maxLength={2} />
+                                    <SmallInput
+                                        maxLength={2}
+                                        onChangeText={setMinute}
+                                    />
                                 </View>
                             </View>
                         </View>
@@ -137,11 +184,13 @@ export function AppointmentCreate() {
                             maxLength={100}
                             numberOfLines={5}
                             autoCorrect={false}
+                            onChangeText={setDescription}
                         />
 
                         <View style={styles.footer}>
                             <Button
                                 title="Agendar"
+                                onPress={handleSave}
                             />
                         </View>
 
